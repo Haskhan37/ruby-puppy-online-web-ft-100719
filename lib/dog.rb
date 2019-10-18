229cc627be68dfdfa43c83653452e9c81c3cc121
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-18 23:53:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,4 +5,15 @@ class Dog
   
   @@all = []
   
-  def
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all.each.do |dog|
+      puts dog.name
+    end
+  end
+  
+  
